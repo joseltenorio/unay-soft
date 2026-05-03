3ea941784ae712b5516c14f9abe9c0337261520e
joseltenorio <jose.tenoriot@unmsm.edu.pe>
2026-05-02 21:44:32 -0500

    refactor: restructure project into monorepo (client/server/docs)

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Steps from './components/Steps'
+import AccountAccess from './components/AccountAccess'
+import Demo from './components/Demo'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Steps />
+      <AccountAccess />
+      <Demo />
+      <Footer />
+    </>
+  )
+}
+
+export default App
