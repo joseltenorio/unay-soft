684d696f17fd83130ca73db6ce9fd54f4cde248b
joseltenorio <jose.tenoriot@unmsm.edu.pe>
2026-05-02 21:44:32 -0500

    refactor: restructure project into monorepo (client/server/docs)

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span>&copy; 2026 UnaySoft. Todos los derechos reservados.</span>
+        <div className="footer__links">
+          <a href="#">Términos</a>
+          <a href="#">Privacidad</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
