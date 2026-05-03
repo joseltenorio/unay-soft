c01c8d20412a93049e96cbb8f197173359dfa561
joseltenorio <jose.tenoriot@unmsm.edu.pe>
2026-05-03 17:00:59 -0500

    feat: configure app routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 // src/App.jsx
-import Home from './pages/Home/Home'
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+
+import Home from "./pages/Home/Home"
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<div>Login pendiente</div>} />
+        <Route path="/restore-password" element={<div>Restablecer contraseña pendiente</div>} />
+        <Route path="/dashboard" element={<div>Dashboard pendiente</div>} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
