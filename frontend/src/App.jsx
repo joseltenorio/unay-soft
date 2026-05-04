c06b47348b8dfa3dbe9c430515c27c1e3b5c84fb
7Alejandra7 <maria.carrascoa@unmsm.edu.pe>
2026-05-04 09:45:23 -0500

    feat: add login and password recovery pages and rename folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,18 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 
 import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import RestorePassword from "./pages/RestorePassword/RestorePassword"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/restore-password" element={<RestorePassword />} />
 
-        <Route path="/login" element={<div>Login pendiente</div>} />
-        <Route path="/restore-password" element={<div>Restablecer contraseña pendiente</div>} />
         <Route path="/dashboard" element={<div>Dashboard pendiente</div>} />
-
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
