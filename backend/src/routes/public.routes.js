8bda8e438af3c619afc72ae60ec6e6963430fc8f
Error504NameNotFound <ana.rojass@unmsm.edu.pe>
2026-07-03 10:23:18 -0500

    AAT-23 feat(routes): apply rate limiter to public menu route

--- backend/src/routes/public.routes.js
+++ backend/src/routes/public.routes.js
@@ -5,16 +5,12 @@ const express = require("express")
 const { cartaPublica, generarQR } = require("../controllers/public.controller")
 const { authenticateToken } = require("../middlewares/auth.middleware")
 const { authorizePermission } = require("../middlewares/permission.middleware")
+const { publicCartaRateLimiter } = require("../middlewares/rateLimit.middleware")
 
 const router = express.Router()
 
-router.get("/carta/:public_identifier", cartaPublica)
+router.get("/carta/:public_identifier", publicCartaRateLimiter, cartaPublica)
 
-router.get(
-  "/qr",
-  authenticateToken,
-  authorizePermission("carta.ver"),
-  generarQR,
-)
+router.get("/qr",authenticateToken,authorizePermission("carta.ver"),generarQR,)
 
 module.exports = router
